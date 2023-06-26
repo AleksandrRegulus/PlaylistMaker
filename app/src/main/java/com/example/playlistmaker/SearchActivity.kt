e332ec6c8263883f3c4587cd44da353968192a70
AleksandrRegulus <kashkarov@mail.ru>
2023-06-26 16:20:51 +0300

    вторая домашка

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,76 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+
+    var searchText = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val btnBack = findViewById<ImageView>(R.id.btn_back)
+        val btnClear = findViewById<ImageButton>(R.id.btn_clear)
+        val searchEditText: EditText = findViewById<EditText>(R.id.search_edit_text)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        btnClear.setOnClickListener {
+            searchEditText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchText = searchEditText.text.toString()
+                btnClear.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        searchEditText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT,searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val searchEditText: EditText = findViewById<EditText>(R.id.search_edit_text)
+
+        searchText = savedInstanceState.getString(SEARCH_TEXT,"")
+        searchEditText.setText(searchText)
+    }
+
 }
