3ab9ae473bf04d6e4420a75b547c13163f5bf484
AleksandrRegulus <kashkarov@mail.ru>
2023-07-06 17:53:46 +0300

    вторая домашка 3

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,5 @@
 package com.example.playlistmaker
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -13,7 +12,44 @@ class SearchActivity : AppCompatActivity() {
 
     private var searchText = ""
     private lateinit var binding: ActivitySearchBinding
-
+    private val tracks = arrayListOf<Track>(
+        Track(
+            "Smells Like Teen Spirit",
+            "Nirvana",
+            "5:01",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Billie Jean",
+            "Michael Jackson",
+            "4:35",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Stayin' Alive",
+            "Bee Gees",
+            "4:10",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Whole Lotta Love",
+            "Led Zeppelin",
+            "5:33",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Длинное имя Длинное имя Длинное имя Длинное имя Длинное имя Длинное имя Длинное имя",
+            "Длинное имя Длинное имя Длинное имя Длинное имя Длинное имя Длинное имя Длинное имя",
+            "0:00",
+            ""
+        ),
+        Track(
+            "Sweet Child O'Mine",
+            "Guns N' Roses",
+            "5:03",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        )
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +62,7 @@ class SearchActivity : AppCompatActivity() {
 
         binding.btnClear.setOnClickListener {
             binding.searchEditText.setText("")
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(binding.searchEditText.windowToken, 0)
         }
 
@@ -45,6 +81,8 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         binding.searchEditText.addTextChangedListener(simpleTextWatcher)
+
+        binding.recyclerView.adapter = TracksAdapter(tracks)
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -57,15 +95,16 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(SEARCH_TEXT,searchText)
+        outState.putString(SEARCH_TEXT, searchText)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
 
-        searchText = savedInstanceState.getString(SEARCH_TEXT,"")
+        searchText = savedInstanceState.getString(SEARCH_TEXT, "")
         binding.searchEditText.setText(searchText)
     }
+
     companion object {
         private const val SEARCH_TEXT = "SEARCH_TEXT"
     }
---
+++ app/src/main/java/com/example/playlistmaker/TracksViewHolder.kt
@@ -0,0 +1,39 @@
+package com.example.playlistmaker
+
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TracksViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    private val trackName: TextView = itemView.findViewById(R.id.trackName)
+    private val artistName: TextView = itemView.findViewById(R.id.artistName)
+    private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+    private val poster: ImageView = itemView.findViewById(R.id.poster)
+
+    fun bind(model: Track) {
+        trackName.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
+
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .placeholder(R.drawable.track_placeholder)
+            .fitCenter()
+            .transform(
+                RoundedCorners(
+                    TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        2f,
+                        itemView.resources.displayMetrics
+                    ).toInt()
+                )
+            )
+            .into(poster)
+    }
+
+}
